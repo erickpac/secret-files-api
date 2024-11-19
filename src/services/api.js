3a500a4b201641fc3592579dbf2f07c7c86f4a81
Erick Pac <erickj.pac@gmail.com>
2024-11-18 18:54:25 -0600

    add axios for API requests and create api service module

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import "dotenv/config";
+
+const { API_URL, AUTH_TOKEN } = process.env;
+
+if (!API_URL || !AUTH_TOKEN) {
+  throw new Error(
+    // eslint-disable-next-line comma-dangle
+    "API_URL and AUTH_TOKEN must be defined in the environment variables"
+  );
+}
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Authorization: AUTH_TOKEN,
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
